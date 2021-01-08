92adf625fe0bf25d8ea54ed7044b57073d005da3
EC2 Default User <ec2-user@ip-172-31-11-47.ec2.internal>
2021-01-08 05:17:25 +0000

    par1 fix

--- par1.py
+++ par1.py
@@ -1,11 +1,10 @@
 import ray
 import time
 
-ray.init(ignore_reinit_error=True, address="auto")
+ray.init(ignore_reinit_error=True)
 
 @ray.remote
 def f(x):
-    time.sleep(1)
     return x*x
 
 futures = [f.remote(i) for i in range(1000)]
